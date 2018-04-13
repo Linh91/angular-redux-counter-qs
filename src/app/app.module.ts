a07e002618eb3a2e3cd3b2124632cb20a1229ac7
sammy.ha <sammy.ha@kingfisher.com>
2018-04-13 16:41:08 +0100

    adding counteraction to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { CounterActions } from './app.actions';
+import { rootReducer, IAppState, INITIAL_STATE } from './../store';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -6,6 +8,7 @@ import { HttpModule } from '@angular/http';
 import { NgReduxModule, NgRedux } from '@angular-redux/store';
 
 import { AppComponent } from './app.component';
+
 
 
 @NgModule({
@@ -18,7 +21,15 @@ import { AppComponent } from './app.component';
     HttpModule,
     NgReduxModule
   ],
-  providers: [],
+  providers: [CounterActions],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    // Tell @angular-redux/store about our rootreducer and our initial state
+    // It will use this to create a redux store for us and wire up all the events
+    ngRedux.configureStore(
+      rootReducer,
+      INITIAL_STATE);
+  }
+ }
