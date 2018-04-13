1f76967e15998d132718ac9ad58ee5ec46f65be0
sammy.ha <sammy.ha@kingfisher.com>
2018-04-13 10:32:45 +0100

    importing ngreduxmodule into application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { NgReduxModule, NgRedux } from '@angular-redux/store';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
