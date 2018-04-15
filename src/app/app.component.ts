3e0ebe8e9fbc79f60a59c18df4b2689573ab2c79
sammy.ha <sammy.ha@kingfisher.com>
2018-04-15 20:37:04 +0100

    added observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { IAppState } from './../store';
+import { Component, OnDestroy } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { CounterActions } from './app.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Counter app';
   count: number;
+  subscription;
 
-  increment() {}
-  decrement() {}
+  constructor(
+    private ngRedux: NgRedux<IAppState>,
+    private actions: CounterActions) {
+      this.subscription = ngRedux.select<number>('count')
+      .subscribe(newCount => this.count = newCount);
+    }
+
+    ngOnDestroy() {
+      this.subscription.unsubscribe();
+    }
+    increment() {
+      this.ngRedux.dispatch(this.actions.increment());
+    }
+    decrement() {
+      this.ngRedux.dispatch(this.actions.decrement());
+    }
 }
+
+// An observable is something that lets you get the latest value of something that changes over time
+// Selected observable will recieve new count each time action happens
